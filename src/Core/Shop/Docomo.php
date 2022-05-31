82ad493ea6ef6fd91daccab0d5b3c5bd7808a7d9
enggar appkey <enggar.appkey@gmail.com>
2022-05-31 16:28:58 +0800

    wip: add validation

--- src/Core/Shop/Docomo.php
+++ src/Core/Shop/Docomo.php
@@ -3,6 +3,7 @@
 namespace Nekoding\GmoPaymentGateway\Core\Shop;
 
 use Closure;
+use Illuminate\Support\Facades\Validator;
 use Nekoding\GmoPaymentGateway\Contracts\Response\ResponseParser;
 use Nekoding\GmoPaymentGateway\Contracts\Shop\Docomo as ShopDocomo;
 use Nekoding\GmoPaymentGateway\Core\InitApiCredentials;
@@ -22,9 +23,16 @@ class Docomo implements ShopDocomo
 
     public function entryTransaction(array $data, ?Closure $callback = null): ResponseParser
     {
+        $params = Validator::make($data, [
+            'OrderID'   => 'required|string|max:27',
+            'JobCd'     => 'required|in:AUTH,CAPTURE',
+            'Amount'    => 'required|numeric|digits:6',
+            'Tax'       => 'nullable|numeric|digits:6',
+        ])->validate();
+
         if ($callback) {
             $this->response = new Response(
-                $this->request(self::ENTRY_TRAN, array_merge($data, $this->getApiCredential()))
+                $this->request(self::ENTRY_TRAN, array_merge($params, $this->getApiCredential()))
             );
 
             return $callback($this);
@@ -35,14 +43,31 @@ class Docomo implements ShopDocomo
 
     public function execTransaction(array $data): ResponseParser
     {
+        $params = Validator::make($data, [
+            'OrderID'       => 'required|string|max:27',
+            'AccessID'      => 'required|string|max:32',
+            'AccessPass'    => 'required|string|max:32',
+            'RetURL'        => 'required|string|max:256',
+            'ClientField1'  => 'nullable|string|max:100',
+            'ClientField2'  => 'nullable|string|max:100',
+            'ClientField3'  => 'nullable|string|max:100',
+            'DocomoDisp1'   => 'nullable|string|max:40',
+            'DocomoDisp2'   => 'nullable|string|max:40',
+            'PaymentTermSec'=> 'nullable|numeric|max:5',
+            'DispShopName'  => 'nullable|string|max:32',
+            'DispPhoneNumber'   => 'nullable|string|max:13',
+            'DispMailAddress'   => 'nullable|string|max:96',
+            'DispShopUrl'   => 'nullable|string|max:96',
+        ])->validate();
+
         if ($this->response) {
-            $data = array_merge($data, $this->response->getResult());
+            $data = array_merge($params, $this->response->getResult());
             return new Response(
-                $this->request(self::EXEC_TRAN, array_merge($data, $this->apiCredential)), $this->response->getResult()
+                $this->request(self::EXEC_TRAN, array_merge($data, $this->getApiCredential())), $this->response->getResult()
             );
         }
 
-        return new Response($this->request(self::EXEC_TRAN, array_merge($data, $this->apiCredential)));
+        return new Response($this->request(self::EXEC_TRAN, array_merge($params, $this->getApiCredential())));
     }
 
 }
